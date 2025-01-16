fb75ca1805a0de7da1a4858bbba51645e9c3f736
Vivek Patel <vivek.patel@eedeecreations.com>
2025-01-16 19:18:13 +0530

    Moblie css fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import HowToParticipate from "./components/HowToParticipate/HowToParticipate";
 import Navbar from "./components/Navbar/Navbar";
 import styles from "./App.module.css";
 export default function App() {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 1024) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener('resize',handleResize)
+    }
+  }, [])
+  
   return (
     <div className={styles.appStructure}>
-      <Navbar />
+      {!isMobile && <Navbar />}
       <div className={styles.innerBody}>
         <Routes>
           <Route index path="/" element={<Home />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import Slider from "../../components/Slider/Slider";
 import assest1 from "../../assets/slider1.png";
 import styles from "./Home.module.css";
@@ -13,103 +13,121 @@ import PanchDinatmak from "../../components/PanchDinatmak/PanchDinatmak.jsx";
 import Particiapte from "../../components/HowToParticipate/HowToParticipate.jsx";
 import Registrationsec from "../../components/registrationsec/registrationsec.jsx";
 import EventBanner from "../../components/EventBanner/EventBanner.jsx";
-import Footer from "../../components/Footer/Footer.jsx"
+import Footer from "../../components/Footer/Footer.jsx";
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+   useEffect(() => {
+      const handleResize = () => {
+        if (window.innerWidth <= 1024) {
+          setIsMobile(true);
+        } else {
+          setIsMobile(false);
+        }
+      };
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener('resize',handleResize)
+      }
+    }, [])
+    
   return (
     <div className={styles.homepageStructure}>
       <div>
-      <Slider assest={assest1} type={"img"} altText={"Section 1"} />
-      <div className={styles.section2}>
-        <div className={styles.section2ImgConatiner}>
-          <div className={styles.section2LeftSideImg}>
-            <img src={Julo} alt="Ghanshyam Julo" />
-          </div>
-          <div className={styles.section2RightSideWrapper}>
-            <div className={styles.section2RightSide}>
-              <div className={styles.HeadersRightSide}>
-                <h1>Why Brahm Mahotsav ?</h1>
-              </div>
-              <div className={styles.ContentRightSide}>
-                <p>
-                  અક્ષરેશ વાત્સલ્યમૂર્તિ મહાપ્રભુ ભગવાન શ્રી સ્વામિનારાયણના
-                  પ્રાગટ્ય પર્વની આરાધન નૃત્ય, પ્રભુ પૂજન સહ ભવ્યાતિભવ્ય ઉજવણીઓ
-                  એટલે બ્રહ્મ મહોત્સવ.
-                </p>
-                <p>
-                  ચૈત્રી નવરાત્રીના પવિત્ર કાળમાં માનવને શક્તિથી સુસજ્જ કરવા....
-                  તપ, જપ, ઝૂલા દ્વારા માનવહૈયામાં પ્રેમના પ્રાગટ્ય અર્થે....
-                  લાખો પરિવારોમાં ભારતીય સાંસ્કૃતિક પરંપરાઓનું સિંચન કરવા....
-                  સાધના,આરાધના, ઉપાસના દ્વારા આધ્યાત્મિક ઉર્જાથી ઉર્જાશીલ માનવના
-                  નિર્માણ....
-                </p>
-                <p>એટલે બ્રહ્મ મહોત્સવ....</p>
-              </div>
-              <div className={styles.WatchNowButtonWrapper}>
-                <button>
-                  <span className="material-symbols-outlined">play_circle</span>{" "}
-                  Watch
-                </button>
+        <Slider assest={assest1} type={"img"} altText={"Section 1"} />
+        <div className={styles.section2}>
+          <div className={styles.section2ImgConatiner}>
+           {!isMobile && <div className={styles.section2LeftSideImg}>
+              <img src={Julo} alt="Ghanshyam Julo" />
+            </div>}
+            <div className={styles.section2RightSideWrapper}>
+              <div className={styles.section2RightSide}>
+                <div className={styles.HeadersRightSide}>
+                  <h1>Why Brahm Mahotsav ?</h1>
+                </div>
+                <div className={styles.ContentRightSide}>
+                  <p>
+                    અક્ષરેશ વાત્સલ્યમૂર્તિ મહાપ્રભુ ભગવાન શ્રી સ્વામિનારાયણના
+                    પ્રાગટ્ય પર્વની આરાધન નૃત્ય, પ્રભુ પૂજન સહ ભવ્યાતિભવ્ય
+                    ઉજવણીઓ એટલે બ્રહ્મ મહોત્સવ.
+                  </p>
+                  <p>
+                    ચૈત્રી નવરાત્રીના પવિત્ર કાળમાં માનવને શક્તિથી સુસજ્જ
+                    કરવા.... તપ, જપ, ઝૂલા દ્વારા માનવહૈયામાં પ્રેમના પ્રાગટ્ય
+                    અર્થે.... લાખો પરિવારોમાં ભારતીય સાંસ્કૃતિક પરંપરાઓનું સિંચન
+                    કરવા.... સાધના,આરાધના, ઉપાસના દ્વારા આધ્યાત્મિક ઉર્જાથી
+                    ઉર્જાશીલ માનવના નિર્માણ....
+                  </p>
+                  <p>એટલે બ્રહ્મ મહોત્સવ....</p>
+                </div>
+                <div className={styles.WatchNowButtonWrapper}>
+                  <button>
+                    <span className="material-symbols-outlined">
+                      play_circle
+                    </span>{" "}
+                    Watch
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.section3Wrapper}>
-        <div className={styles.section3}>
-          <h1>Live Event</h1>
-          <div className={styles.section3LiveEventBox}>
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/jsmmn4obFAw?si=YhimR65oeSBOtyb3"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
-          </div>
-          <div className={styles.LiveEventFooter}>
-            <div className={styles.LiveEventFooterImgDesign}>
-              <img src={Vector} alt="design svg" />
+        <div className={styles.section3Wrapper}>
+          <div className={styles.section3}>
+            <h1>Live Event</h1>
+            <div className={styles.section3LiveEventBox}>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/jsmmn4obFAw?si=YhimR65oeSBOtyb3"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
             </div>
-            <div className={styles.LiveEventFooterContent}>
-              <h1>2024</h1>
-              <img height={"120"} src={Line} alt="Line" />
-              <p>
-                Brahm Mahotsav <br /> Celebrations
+            <div className={styles.LiveEventFooter}>
+              <div className={styles.LiveEventFooterImgDesign}>
+                <img src={Vector} alt="design svg" />
+              </div>
+              <div className={styles.LiveEventFooterContent}>
+                <h1>2024</h1>
+                {!isMobile && <img height={"120"} src={Line} alt="Line" />}
+                <p>
+                  Brahm Mahotsav <br /> Celebrations
+                </p>
+              </div>{" "}
+              <br />
+              <p style={{ textAlign: "center",maxWidth:"100vw",fontSize:"2vh" }}>
+                ભગવાન શ્રી સ્વામિનારાયણના પ્રાગટ્યોત્સવ પર્વે હર ઘર ઝૂલા,
+                પૂજનોત્સવ તથા આરાધન નૃત્ય દ્વારા <br />
+                ભક્તો બ્રહ્મ મહોત્સવની ઉજવણી કરશે
               </p>
-            </div>{" "}
-            <br />
-            <p style={{ textAlign: "center" }}>
-              ભગવાન શ્રી સ્વામિનારાયણના પ્રાગટ્યોત્સવ પર્વે હર ઘર ઝૂલા,
-              પૂજનોત્સવ તથા આરાધન નૃત્ય દ્વારા <br />
-              ભક્તો બ્રહ્મ મહોત્સવની ઉજવણી કરશે
-            </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.section4}>
-        <div className={styles.imagesWrapper}>
-          <div className={styles.imgBox}>
-            <img src={group1} alt="group 1" />
-          </div>
-          <div className={styles.imgBox}>
-            <img src={group2} alt="group 2" />
-          </div>
-          <div className={styles.imgBox}>
-            <img src={group3} alt="group 3" />
-          </div>
-          <div className={styles.imgBox}>
-            <img src={group4} alt="group 4" />
+        <div className={styles.section4}>
+          <div className={styles.imagesWrapper}>
+            <div className={styles.imgBox}>
+              <img src={group1} alt="group 1" />
+            </div>
+            <div className={styles.imgBox}>
+              <img src={group2} alt="group 2" />
+            </div>
+            <div className={styles.imgBox}>
+              <img src={group3} alt="group 3" />
+            </div>
+            <div className={styles.imgBox}>
+              <img src={group4} alt="group 4" />
+            </div>
           </div>
         </div>
-      </div>
         <PanchDinatmak />
-      <Particiapte />
-      <Registrationsec />
-      <EventBanner />
-      <Footer/>
+        <Particiapte />
+        <Registrationsec />
+        <EventBanner />
+        <Footer />
       </div>
     </div>
   );
